1e46cdcedbe65794bbb8a879e470bd3f7ba06774
ksjc1995 <dev.darkmatter69@gmail.com>
2019-06-02 11:56:24 +0530

    added question & answers

--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -1,62 +1,62 @@
 const questions = [
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Which amongst the following is the largest mammal?",
+        ans:"WHALE",
         level:1
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Which animal exclusively eat only eucalyptus leaves and nothing else",
+        ans:"KOALA",
         level:1
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"The animal having longest life span?",
+        ans:"TORTOISE",
         level:1
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Which is the largest living bird on Earth?",
+        ans:"OSTRICH",
         level:1
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"What was the name of the first cloned sheep?",
+        ans:"DOLLY",
         level:2
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Pashmina shawl is made from the hair of?",
+        ans:"GOAT",
         level:2
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"In which of the following animals is respiration done by skin?",
+        ans:"DOLPHIN",
         level:2
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Which animal do not make any sound?",
+        ans:"GIRAFFE",
         level:2
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Name the only animal that dislikes white colour?",
+        ans:"TIGER",
         level:3
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"Which of the following has the largest eyes of all land mammals?",
+        ans:"HORSE",
         level:3
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"What is a rhinos horn made of ?",
+        ans:"KERATIN",
         level:3
     },
     {
-        question:"Sample Question 1",
-        ans:"SAMPLE",
+        question:"In which of the following animals is respiration done by skin?",
+        ans:"FROG",
         level:3
     },
 ]
